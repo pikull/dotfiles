7ec5e2ba335adaa7b57c469b2c115968711f5d91
pikull <118777590+pikull@users.noreply.github.com>
2025-07-22 11:14:12 -0700

    formatting

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             local builtin = require("telescope.builtin")
             local opts = { noremap = true, silent = true }
-            vim.keymap.set('n', "<leader>ff", builtin.find_files, opts) 
+            vim.keymap.set('n', "<leader>ff", builtin.find_files, opts)
             vim.keymap.set('n', "<leader>fg", builtin.live_grep, opts)
         end
     },
