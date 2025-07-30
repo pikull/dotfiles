408213a8c959546bfbe44d4a49a3d88d9e72dc76
pikull <118777590+pikull@users.noreply.github.com>
2025-07-30 15:29:15 -0700

    skip confirm for simple edits

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,19 +1,20 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		---@module 'oil'
-		---@type oil.SetupOpts
-		keys = {
-			{ "<leader>e", "<cmd>Oil<CR>", desc = "Open parent directory" },
-		},
-		opts = {
-			default_file_explorer = true,
-			view_options = {
-				show_hidden = true,
-			},
-		},
-		-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		lazy = false,
-	},
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Open parent directory" },
+    },
+    opts = {
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    lazy = false,
+  },
 }
