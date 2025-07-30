df346c54c04d54d4df6b24d1d63c4a6afb1e1d6a
pikull <118777590+pikull@users.noreply.github.com>
2025-07-30 14:23:38 -0700

    set tab size 2 for certain file types

--- nvim/.config/nvim/lua/autos.lua
+++ nvim/.config/nvim/lua/autos.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "javascript", "typescript", "javascriptreact", "json", "html", "css", "lua" },
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
+})
