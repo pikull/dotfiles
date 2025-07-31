db5213e5bd4e8ecdc606841318a226c1d504c257
pikull <118777590+pikull@users.noreply.github.com>
2025-07-31 12:39:01 -0700

    2 space indent for .tsx

--- nvim/.config/nvim/lua/autos.lua
+++ nvim/.config/nvim/lua/autos.lua
@@ -1,16 +1,16 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("HighlightYank", {}),
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
-	end,
+  group = vim.api.nvim_create_augroup("HighlightYank", {}),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "javascript", "typescript", "javascriptreact", "json", "html", "css", "lua" },
-	callback = function()
-		vim.opt_local.tabstop = 2
-		vim.opt_local.softtabstop = 2
-		vim.opt_local.shiftwidth = 2
-	end,
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "json", "html", "css", "lua" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
 })
