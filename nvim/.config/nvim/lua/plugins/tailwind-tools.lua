f08f2602435b747125714848f260f8114dc71d6f
pikull <118777590+pikull@users.noreply.github.com>
2025-08-05 14:28:31 -0700

    added css tailwind completion

---
+++ nvim/.config/nvim/lua/plugins/tailwind-tools.lua
@@ -0,0 +1,10 @@
+-- tailwind-tools.lua
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {}, -- your configuration
+}
