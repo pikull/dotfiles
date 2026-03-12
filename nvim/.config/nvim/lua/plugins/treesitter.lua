ce8dbf8c41aefa3e368b0737a2d4fcc9885a4bb6
Akshun Chinara <118777590+pikull@users.noreply.github.com>
2026-03-12 09:53:51 -0700

    update treesitter config for new update

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,40 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true, disable = { "c", "cpp" } },
+      local ts = require("nvim-treesitter")
+
+      ts.install({
+        "python",
+        "c",
+        "cpp",
+        "java",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "lua",
+        "vim",
+        "vimdoc",
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "Enable Treesitter syntax highlighting natively",
+        callback = function()
+          -- pcall prevents errors from throwing if a parser isn't installed yet
+          pcall(vim.treesitter.start) 
+        end,
+      })
+
+      -- This replicates your old rule of enabling indent, but disabling for C/C++
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "Enable Treesitter indentation (with exclusions)",
+        callback = function(args)
+          local disabled_langs = { c = true, cpp = true }
+          if not disabled_langs[args.match] then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
