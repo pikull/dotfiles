37cfb9e633a37b514f5d83c6fdda930fda794ae1
Akshun Chinara <118777590+pikull@users.noreply.github.com>
2026-06-07 02:27:26 -0700

    lspconfig

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -41,6 +41,30 @@ return {
 			"saghen/blink.cmp",
 		},
 		config = function()
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●",
+					spacing = 4,
+					source = "if_many",
+				},
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "󰅚 ",
+						[vim.diagnostic.severity.WARN] = "󰀪 ",
+						[vim.diagnostic.severity.INFO] = " ",
+						[vim.diagnostic.severity.HINT] = "󰌶 ",
+					},
+				},
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+				float = {
+					border = "rounded",
+					source = true,
+					header = "",
+					prefix = "",
+				},
+			})
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("LspConfig", {}),
 				callback = function(ev)
